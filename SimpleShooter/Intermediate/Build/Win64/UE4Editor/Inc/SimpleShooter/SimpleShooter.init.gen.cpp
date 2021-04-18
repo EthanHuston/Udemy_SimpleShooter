42d7cf8cf709a9e992a77b5ee2044bc3b1fd63bd
Ethan Huston <ethanghuston@gmail.com>
2021-04-18 16:42:22 -0400

    Created Custom BTTasks in C++, making Enemy AI a bit better

--- SimpleShooter/Intermediate/Build/Win64/UE4Editor/Inc/SimpleShooter/SimpleShooter.init.gen.cpp
+++ SimpleShooter/Intermediate/Build/Win64/UE4Editor/Inc/SimpleShooter/SimpleShooter.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeSimpleShooter_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x8B6ACB25,
-				0x3CBC390F,
+				0x650FB0BE,
+				0xA8B8224D,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
